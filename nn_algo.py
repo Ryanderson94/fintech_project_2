5a74f1fca5aae127ec2d23360fb206c29d4b57ec
Ryanderson94 <m.anderson.ryan@gmail.com>
2022-07-03 14:18:38 -0400

    Created functions for algos

--- nn_algo.py
+++ nn_algo.py
@@ -17,153 +17,162 @@ alpaca_api_key = get_alpacas_info()[1]
 alpaca_secret_key = get_alpacas_info()[2]
 
 # define bunch of variables that can be easiy changed to fine turn the algo
-stock = 'SPY' ### THIS IS WHERE WE NEED TO INPUT THE STOCK ###
-shift_time = 1
-start_date_historical = '2022-06-07'
-end_date_historical = '2022-06-10'
-periods_for_training_data = 800
-sma_short_window =20
-sma_long_window=50
-RSI_time_period = 3
-EMA_time_period = 50
+def define_hyper_parameters():
+    stock = 'SPY' ### THIS IS WHERE WE NEED TO INPUT THE STOCK ###
+    shift_time = 1
+    start_date_historical = '2022-06-07'
+    end_date_historical = '2022-06-10'
+    periods_for_training_data = 800
+    sma_short_window =20
+    sma_long_window=50
+    RSI_time_period = 3
+    EMA_time_period = 50
 
-# Define alpaca api object 
-bars = api.get_bars("SPY", TimeFrame.Minute, start_date_historical, end_date_historical).df
+    return stock, shift_time, start_date_historical, end_date_historical, periods_for_training_data, sma_short_window, sma_long_window, RSI_time_period, EMA_time_period
 
-bars["actual_returns"] = bars["close"].pct_change()
+def run_trading_bot(
+    stock, 
+    shift_time, 
+    start_date_historical, 
+    end_date_historical, 
+    periods_for_training_data, 
+    sma_short_window, 
+    sma_long_window, 
+    RSI_time_period, 
+    EMA_time_period
+    ):
+    
+    # Define alpaca api object 
+    bars = api.get_bars("SPY", TimeFrame.Minute, start_date_historical, end_date_historical).df
 
-# Drop all NaN values from the DataFrame
-bars = bars.dropna()
+    bars["actual_returns"] = bars["close"].pct_change()
 
-# use talib to calcualte SMA (Simple moving avera
-bars['sma_short_window'] = ta.SMA(bars['close'],timeperiod=sma_short_window)
-bars['sma_long_window'] = ta.SMA(bars['close'],timeperiod=sma_long_window)
+    # Drop all NaN values from the DataFrame
+    bars = bars.dropna()
 
-bars['RSI'] = ta.RSI(bars['close'],timeperiod=RSI_time_period)
-bars['EMA'] = ta.RSI(bars['close'],timeperiod=EMA_time_period)
+    # use talib to calcualte SMA (Simple moving avera
+    bars['sma_short_window'] = ta.SMA(bars['close'],timeperiod=sma_short_window)
+    bars['sma_long_window'] = ta.SMA(bars['close'],timeperiod=sma_long_window)
 
-bars = bars.dropna()
+    bars['RSI'] = ta.RSI(bars['close'],timeperiod=RSI_time_period)
+    bars['EMA'] = ta.RSI(bars['close'],timeperiod=EMA_time_period)
 
-# Assign a copy of the technical variable columns to a new DataFrame called `X` and lag it.
-X = bars[['sma_short_window', 'sma_long_window', 'RSI', 'EMA', ]].shift(shift_time).dropna().copy()
+    bars = bars.dropna()
 
-# Initialize the new `Signal` column
-bars['signal'] = 0.0
+    # Assign a copy of the technical variable columns to a new DataFrame called `X` and lag it.
+    X = bars[['sma_short_window', 'sma_long_window', 'RSI', 'EMA', ]].shift(shift_time).dropna().copy()
 
-# Generate signal to buy stock long
-bars.loc[(bars['actual_returns'] >= 0), 'signal'] = 1
+    # Initialize the new `Signal` column
+    bars['signal'] = 0.0
 
-# Generate signal to sell stock short
-bars.loc[(bars['actual_returns'] < 0), 'signal'] = -1
+    # Generate signal to buy stock long
+    bars.loc[(bars['actual_returns'] >= 0), 'signal'] = 1
 
-bars['contrarian_signal']= bars['signal']*(-1)
+    # Generate signal to sell stock short
+    bars.loc[(bars['actual_returns'] < 0), 'signal'] = -1
 
-# Calculate the strategy returns and add them to the signals_df DataFrame
-# add shift after signal is because the buying/selling will be one day delay after the reutrn
-bars['Strategy_Returns'] = bars['actual_returns'] * bars['signal'].shift()
-bars['contrarian_Strategy_Returns'] = bars['actual_returns'] * bars['contrarian_signal'].shift()
+    bars['contrarian_signal']= bars['signal']*(-1)
 
-# Copy the new "signal" column to a new Series called `y`.
-y = bars['signal']
+    # Calculate the strategy returns and add them to the signals_df DataFrame
+    # add shift after signal is because the buying/selling will be one day delay after the reutrn
+    bars['Strategy_Returns'] = bars['actual_returns'] * bars['signal'].shift()
+    bars['contrarian_Strategy_Returns'] = bars['actual_returns'] * bars['contrarian_signal'].shift()
 
-# Import the neccessary Date function
-from pandas.tseries.offsets import DateOffset
+    # Copy the new "signal" column to a new Series called `y`.
+    y = bars['signal']
 
-# Use the following code to select the start of the training period: `training_begin = X.index.min()`
-training_begin = X.index.min()
-print(training_begin)
+    # Import the neccessary Date function
+    from pandas.tseries.offsets import DateOffset
 
-# Use the following code to select the ending period for the training data: `training_end = X.index.min() + DateOffset(months=3)`
-training_end = X.index.min() + DateOffset(minutes=periods_for_training_data)
-print(training_end)
+    # Use the following code to select the start of the training period: `training_begin = X.index.min()`
+    training_begin = X.index.min()
+    print(training_begin)
 
-# Generate the X_train and y_train DataFrames using loc to select the rows from `training_begin` up to `training_end`
-X_train = X.loc[training_begin:training_end]
-y_train = y.loc[training_begin:training_end]
+    # Use the following code to select the ending period for the training data: `training_end = X.index.min() + DateOffset(months=3)`
+    training_end = X.index.min() + DateOffset(minutes=periods_for_training_data)
+    print(training_end)
 
-# Generate the X_test and y_test DataFrames using loc to select from `training_end` to the last row in the DataFrame.
-X_test = X.loc[training_end:]
-y_test = y.loc[training_end:]
+    # Generate the X_train and y_train DataFrames using loc to select the rows from `training_begin` up to `training_end`
+    X_train = X.loc[training_begin:training_end]
+    y_train = y.loc[training_begin:training_end]
 
-# Use StandardScaler to scale the X_train and X_test data.
-from sklearn.preprocessing import StandardScaler
-scaler = StandardScaler()
-X_scaler = scaler.fit(X_train)
-X_train_scaled = X_scaler.transform(X_train)
-X_test_scaled = X_scaler.transform(X_test)
-X_test_scaled.shape
+    # Generate the X_test and y_test DataFrames using loc to select from `training_end` to the last row in the DataFrame.
+    X_test = X.loc[training_end:]
+    y_test = y.loc[training_end:]
 
-from imblearn.over_sampling import RandomOverSampler
+    # Use StandardScaler to scale the X_train and X_test data.
+    from sklearn.preprocessing import StandardScaler
+    scaler = StandardScaler()
+    X_scaler = scaler.fit(X_train)
+    X_train_scaled = X_scaler.transform(X_train)
+    X_test_scaled = X_scaler.transform(X_test)
+    X_test_scaled.shape
 
-# Use RandomOverSampler to resample the datase using random_state=1
-ros = RandomOverSampler(random_state=1)
-X_resampled, y_resampled = ros.fit_resample(X_train_scaled, y_train)
+    from imblearn.over_sampling import RandomOverSampler
 
-# Define the the number of inputs
-number_input_features = 4
+    # Use RandomOverSampler to resample the datase using random_state=1
+    ros = RandomOverSampler(random_state=1)
+    X_resampled, y_resampled = ros.fit_resample(X_train_scaled, y_train)
 
-# Review the number of features
-number_input_features
+    # Define the the number of inputs
+    number_input_features = 4
 
-# Define the number of neurons in the output layer
-number_output_neurons = 1
+    # Define the number of neurons in the output layer
+    number_output_neurons = 1
 
-# Define the number of hidden nodes for the first hidden layer
-hidden_nodes_layer1 =  8
+    # Define the number of hidden nodes for the first hidden layer
+    hidden_nodes_layer1 =  8
 
-# Review the number hidden nodes in the first layer
-hidden_nodes_layer1
+    nn = Sequential()
 
-nn = Sequential()
+    # First hidden layer
+    nn.add(Dense(units=hidden_nodes_layer1,input_dim = number_input_features,activation='relu'))
 
-# First hidden layer
-nn.add(Dense(units=hidden_nodes_layer1,input_dim = number_input_features,activation='relu'))
+    # Output layer
+    nn.add(Dense(units=number_output_neurons, activation='sigmoid'))
 
-# Output layer
-nn.add(Dense(units=number_output_neurons, activation='sigmoid'))
+    # Check the structure of the model
+    nn.summary()
 
-# Check the structure of the model
-nn.summary()
+    # Compile the model
+    nn.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
 
-# Compile the model
-nn.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+    # Fit the model
+    # X_resampled, y_resampled
+    fit_model = nn.fit(X_train_scaled,y_train,epochs=100)
 
-# Fit the model
-# X_resampled, y_resampled
-fit_model = nn.fit(X_train_scaled,y_train,epochs=100)
+    # Evaluate the model loss and accuracy metrics using the evaluate method and the test data
+    model_loss, model_accuracy = nn.evaluate(X_test_scaled, y_test, verbose=2)
 
-# Evaluate the model loss and accuracy metrics using the evaluate method and the test data
-model_loss, model_accuracy = nn.evaluate(X_test_scaled, y_test, verbose=2)
+    # Display the model loss and accuracy results
+    print(f"Loss: {model_loss}, Accuracy: {model_accuracy}")
 
-# Display the model loss and accuracy results
-print(f"Loss: {model_loss}, Accuracy: {model_accuracy}")
+    # Use the trained model to predict the trading signals for the training data.
+    training_signal_predictions = nn.predict(X_test_scaled)
 
-# Use the trained model to predict the trading signals for the training data.
-training_signal_predictions = nn.predict(X_test_scaled)
+    contrarian_training_signal_predictions = training_signal_predictions * (-1)
 
-contrarian_training_signal_predictions = training_signal_predictions * (-1)
+    # Create a predictions DataFrame
+    predictions_df = pd.DataFrame(index=X_test.index)
 
-# Create a predictions DataFrame
-predictions_df = pd.DataFrame(index=X_test.index)
+    # Add the SVM model predictions to the DataFrame
+    predictions_df['Predicted'] = training_signal_predictions
 
-# Add the SVM model predictions to the DataFrame
-predictions_df['Predicted'] = training_signal_predictions
-
-predictions_df['Contrarian_Predicted'] = contrarian_training_signal_predictions
+    predictions_df['Contrarian_Predicted'] = contrarian_training_signal_predictions
 
 
-# Add the actual returns to the DataFrame
-predictions_df['actual_returns'] = bars['actual_returns'] 
+    # Add the actual returns to the DataFrame
+    predictions_df['actual_returns'] = bars['actual_returns'] 
 
-# Add the strategy returns to the DataFrame
-predictions_df['Strategy_Returns'] = (
-    predictions_df["actual_returns"] * predictions_df["Predicted"]
-)
+    # Add the strategy returns to the DataFrame
+    predictions_df['Strategy_Returns'] = (
+        predictions_df["actual_returns"] * predictions_df["Predicted"]
+    )
 
-predictions_df['Strategy_Returns'] = (
-    predictions_df["actual_returns"] * predictions_df["Predicted"]
-)
+    predictions_df['Strategy_Returns'] = (
+        predictions_df["actual_returns"] * predictions_df["Predicted"]
+    )
 
-predictions_df['Contrarian_Strategy_Returns'] = (
-    predictions_df["actual_returns"] * predictions_df["Contrarian_Predicted"]
-)
+    predictions_df['Contrarian_Strategy_Returns'] = (
+        predictions_df["actual_returns"] * predictions_df["Contrarian_Predicted"]
+    )
--- svm_algo.py
+++ svm_algo.py
@@ -17,160 +17,169 @@ alpaca_api_key = get_alpacas_info()[1]
 alpaca_secret_key = get_alpacas_info()[2]
 
 # use timedelta from Datetime to generate earlier date so we can extract historical data from alpaca later
-days_to_subtract = 7
-today = (datetime.today()).strftime('%Y-%m-%d')
 
-earlier_date_to_compare = (datetime.today()-timedelta(days=days_to_subtract)).strftime('%Y-%m-%d')
+def define_hyper_parameters():
+    days_to_subtract = 7
+    today = (datetime.today()).strftime('%Y-%m-%d')
 
-# Define parameters for the algorithm
-stock = 'SPY'                                       # Stock ticker
-shift_time = 1                                      # Offset shift
-start_date_historical = earlier_date_to_compare     # Historical start date
-end_date_historical = today                         # Historical end date
-periods_for_training_data = 800                     # Period count for training data
-sma_short_window = 20                               # Simple Moving Average short window length
-sma_long_window = 50                                # Simple Moving Average long window length
-RSI_time_period = 3                                 # RSI Time Period
-EMA_time_period = 50                                # EMA Time Period
+    earlier_date_to_compare = (datetime.today()-timedelta(days=days_to_subtract)).strftime('%Y-%m-%d')
+
+    # Define parameters for the algorithm
+    stock = 'SPY'                                       # Stock ticker
+    shift_time = 1                                      # Offset shift
+    start_date_historical = earlier_date_to_compare     # Historical start date
+    end_date_historical = today                         # Historical end date
+    periods_for_training_data = 800                     # Period count for training data
+    sma_short_window = 20                               # Simple Moving Average short window length
+    sma_long_window = 50                                # Simple Moving Average long window length
+    RSI_time_period = 3                                 # RSI Time Period
+    EMA_time_period = 50                                # EMA Time Period
+
+    return stock, shift_time, start_date_historical, end_date_historical, periods_for_training_data, sma_short_window, sma_long_window, RSI_time_period, EMA_time_period
 
 # Run the signal function
-while True:
-    bars = api.get_bars(stock, TimeFrame.Minute, start_date_historical, end_date_historical).df
-    bars['actual_returns'] = bars['close'].pct_change()
-    bars = bars.dropna()
-    
-    # Use ta-lib to calcualte the simple moving average (SMA)
-    bars['sma_short_window'] = ta.SMA(bars['close'],timeperiod=sma_short_window)
-    bars['sma_long_window'] = ta.SMA(bars['close'],timeperiod=sma_long_window)
+def run_trading_bot(
+    stock, 
+    shift_time, 
+    start_date_historical, 
+    end_date_historical, 
+    periods_for_training_data, 
+    sma_short_window, 
+    sma_long_window, 
+    RSI_time_period, 
+    EMA_time_period
+    ):
 
-    bars['RSI'] = ta.RSI(bars['close'],timeperiod=RSI_time_period)
-    bars['EMA'] = ta.RSI(bars['close'],timeperiod=EMA_time_period)
+    while True:
+        bars = api.get_bars(stock, TimeFrame.Minute, start_date_historical, end_date_historical).df
+        bars['actual_returns'] = bars['close'].pct_change()
+        bars = bars.dropna()
+        
+        # Use ta-lib to calcualte the simple moving average (SMA)
+        bars['sma_short_window'] = ta.SMA(bars['close'],timeperiod=sma_short_window)
+        bars['sma_long_window'] = ta.SMA(bars['close'],timeperiod=sma_long_window)
 
-    # Drop NaN values in columns
-    bars = bars.dropna()
+        bars['RSI'] = ta.RSI(bars['close'],timeperiod=RSI_time_period)
+        bars['EMA'] = ta.RSI(bars['close'],timeperiod=EMA_time_period)
 
-    # Assign a copy of the technical variable columns to the Features dataframe and lag it
-    X = bars[['sma_short_window', 'sma_long_window', 'RSI', 'EMA', ]].shift(shift_time).dropna().copy()
-    
-    # Assign a copy of technical varaible columns to the Trading Features dataframe and lag it
-    X_trading = bars_trading[['sma_short_window', 'sma_long_window', 'RSI', 'EMA', ]].shift(shift_time).dropna().copy()
+        # Drop NaN values in columns
+        bars = bars.dropna()
 
-    # Create a new signal column and default value to '0'
-    bars['signal'] = 0.0
+        # Assign a copy of the technical variable columns to the Features dataframe and lag it
+        X = bars[['sma_short_window', 'sma_long_window', 'RSI', 'EMA', ]].shift(shift_time).dropna().copy()
 
-    # Generate signal to buy stock long
-    bars.loc[(bars['actual_returns'] >= 0), 'signal'] = 1
+        # Create a new signal column and default value to '0'
+        bars['signal'] = 0.0
 
-    # Generate signal to sell stock short
-    bars.loc[(bars['actual_returns'] < 0), 'signal'] = -1
-    
-    # Set contrarian signal as inverse of signal generated based on actual returns
-    bars['contrarian_signal']= bars['signal']*(-1)
-    
-    # Calculate the strategy returns and add them to the signals_df DataFrame
-    # add shift after signal is because the buying/selling will be one day delay after the reutrn
-    bars['Strategy_Returns'] = bars['actual_returns'] * bars['signal'].shift(shift_time)
-    bars['contrarian_Strategy_Returns'] = bars['actual_returns'] * bars['contrarian_signal'].shift(shift_time)
+        # Generate signal to buy stock long
+        bars.loc[(bars['actual_returns'] >= 0), 'signal'] = 1
 
-    # We can probably get rid of this, i assume
-    # (1 + bars['Strategy_Returns']).cumprod().plot()
-    # (1 + bars['contrarian_Strategy_Returns']).cumprod().plot()
-    
-    # Copy the "signal" column to the target series called `y`
-    y = bars['signal']
+        # Generate signal to sell stock short
+        bars.loc[(bars['actual_returns'] < 0), 'signal'] = -1
+        
+        # Set contrarian signal as inverse of signal generated based on actual returns
+        bars['contrarian_signal']= bars['signal']*(-1)
+        
+        # Calculate the strategy returns and add them to the signals_df DataFrame
+        # add shift after signal is because the buying/selling will be one day delay after the reutrn
+        bars['Strategy_Returns'] = bars['actual_returns'] * bars['signal'].shift(shift_time)
+        bars['contrarian_Strategy_Returns'] = bars['actual_returns'] * bars['contrarian_signal'].shift(shift_time)
+        
+        # Copy the "signal" column to the target series called `y`
+        y = bars['signal']
 
-    # Set the start and end of the training period
-    training_begin = X.index.min()
-    training_end = X.index.min() + DateOffset(minutes=periods_for_training_data)
-    
-    # Generate the X_train and y_train DataFrames
-    X_train = X.loc[training_begin:training_end]
-    y_train = y.loc[training_begin:training_end]
+        # Set the start and end of the training period
+        training_begin = X.index.min()
+        training_end = X.index.min() + DateOffset(minutes=periods_for_training_data)
+        
+        # Generate the X_train and y_train DataFrames
+        X_train = X.loc[training_begin:training_end]
+        y_train = y.loc[training_begin:training_end]
 
-    # Generate the X_test and y_test DataFrames
-    X_test = X.loc[training_end:]
-    y_test = y.loc[training_end:]
-    
-    # Scale the training and testing data
-    from sklearn.preprocessing import StandardScaler
-    scaler = StandardScaler()
-    
-    X_scaler = scaler.fit(X_train)
-    
-    X_train_scaled = X_scaler.transform(X_train)
-    
-    X_test_scaled = X_scaler.transform(X_test)
-    
-    X_trading_scaled = X_scaler.transform(X_trading)
+        # Generate the X_test and y_test DataFrames
+        X_test = X.loc[training_end:]
+        y_test = y.loc[training_end:]
+        
+        # Scale the training and testing data
+        from sklearn.preprocessing import StandardScaler
+        scaler = StandardScaler()
+        
+        X_scaler = scaler.fit(X_train)
+        
+        X_train_scaled = X_scaler.transform(X_train)
+        
+        X_test_scaled = X_scaler.transform(X_test)
+        
+        X_trading_scaled = X_scaler.transform(X_trading)
 
-    from imblearn.over_sampling import RandomOverSampler
-    
-    # Resample the data
-    ros = RandomOverSampler(random_state=1)
-    X_resampled, y_resampled = ros.fit_resample(X_train_scaled, y_train)
+        from imblearn.over_sampling import RandomOverSampler
+        
+        # Resample the data
+        ros = RandomOverSampler(random_state=1)
+        X_resampled, y_resampled = ros.fit_resample(X_train_scaled, y_train)
 
-    # Create the classifier model.
-    from sklearn.svm import SVC
-    model = SVC()
+        # Create the classifier model.
+        from sklearn.svm import SVC
+        model = SVC()
 
-    # Fit the model to the data using X_train_scaled and y_train
-    model = model.fit(X_resampled, y_resampled)
-    
-    # Use the trained model to predict the trading signals for the training data.
-    training_signal_predictions = model.predict(X_resampled)
+        # Fit the model to the data using X_train_scaled and y_train
+        model = model.fit(X_resampled, y_resampled)
+        
+        # Use the trained model to predict the trading signals for the training data.
+        training_signal_predictions = model.predict(X_resampled)
 
-    # Evaluate the model using a classification report
-    from sklearn.metrics import classification_report
-    training_report = classification_report(y_resampled, training_signal_predictions)
-    print(training_report)
+        # Evaluate the model using a classification report
+        from sklearn.metrics import classification_report
+        training_report = classification_report(y_resampled, training_signal_predictions)
+        print(training_report)
 
-    # Use the trained model to predict the trading signals for the training data.
-    training_signal_predictions = model.predict(X_test_scaled)
+        # Use the trained model to predict the trading signals for the training data.
+        training_signal_predictions = model.predict(X_test_scaled)
 
-    # NEED COMMENT HERE
-    contrarian_training_signal_predictions = training_signal_predictions * (-1)
+        # NEED COMMENT HERE
+        contrarian_training_signal_predictions = training_signal_predictions * (-1)
 
-    # Create a predictions DataFrame
-    predictions_df = pd.DataFrame(index=X_test.index)
+        # Create a predictions DataFrame
+        predictions_df = pd.DataFrame(index=X_test.index)
 
-    # Add the SVM model predictions to the DataFrame
-    predictions_df['Predicted'] = training_signal_predictions
+        # Add the SVM model predictions to the DataFrame
+        predictions_df['Predicted'] = training_signal_predictions
 
-    # NEED COMMENT HERE
-    predictions_df['Contrarian_Predicted'] = contrarian_training_signal_predictions
+        # NEED COMMENT HERE
+        predictions_df['Contrarian_Predicted'] = contrarian_training_signal_predictions
 
-    # Add the actual returns to the DataFrame
-    predictions_df['actual_returns'] = bars['actual_returns'] 
+        # Add the actual returns to the DataFrame
+        predictions_df['actual_returns'] = bars['actual_returns'] 
 
-    # Add the strategy returns to the DataFrame
-    predictions_df['Strategy_Returns'] = (
-        predictions_df["actual_returns"] * predictions_df["Predicted"]
-    )
+        # Add the strategy returns to the DataFrame
+        predictions_df['Strategy_Returns'] = (
+            predictions_df["actual_returns"] * predictions_df["Predicted"]
+        )
 
-    # COMMENT
-    predictions_df['Strategy_Returns'] = (
-        predictions_df["actual_returns"] * predictions_df["Predicted"]
-    )
+        # COMMENT
+        predictions_df['Strategy_Returns'] = (
+            predictions_df["actual_returns"] * predictions_df["Predicted"]
+        )
 
-    # COMMENT
-    predictions_df['Contrarian_Strategy_Returns'] = (
-        predictions_df["actual_returns"] * predictions_df["Contrarian_Predicted"]
-    )
+        # COMMENT
+        predictions_df['Contrarian_Strategy_Returns'] = (
+            predictions_df["actual_returns"] * predictions_df["Contrarian_Predicted"]
+        )
 
-    # Extract trading signal from the last min in the df
-    trading_execute_signal = predictions_df.iloc[-1,0]
-    trading_execute_signal
-    if trading_execute_signal >0:
-        side = 'buy'
-        side_reverse = 'sell'
-    else: 
-        side = 'sell'
-        side_reverse = 'buy'
+        # Extract trading signal from the last min in the df
+        trading_execute_signal = predictions_df.iloc[-1,0]
+        trading_execute_signal
+        if trading_execute_signal >0:
+            side = 'buy'
+            side_reverse = 'sell'
+        else: 
+            side = 'sell'
+            side_reverse = 'buy'
 
-    print('side is',side)
-    print('side_reverse is',side_reverse)
-    
-    """This is the Trading Bot Piece"""
-    api.submit_order(symbol=stock,qty=1,side=side,type='market',time_in_force='gtc')
-    time.sleep(60)
-    api.submit_order(symbol=stock,qty=1,side=side_reverse,type='market',time_in_force='gtc')
+        print('side is',side)
+        print('side_reverse is',side_reverse)
+        
+        """This is the Trading Bot Piece"""
+        api.submit_order(symbol=stock,qty=1,side=side,type='market',time_in_force='gtc')
+        time.sleep(60)
+        api.submit_order(symbol=stock,qty=1,side=side_reverse,type='market',time_in_force='gtc')
